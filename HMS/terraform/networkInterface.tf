b8ddb5489cfac729af380d05dda3f6c03d827dc8
Manjunath Nayak <manjunathnayak2311@gmail.com>
2023-04-19 17:19:03 +0530

    initial commit

--- HMS/terraform/networkInterface.tf
+++ HMS/terraform/networkInterface.tf
@@ -7,5 +7,16 @@ resource "azurerm_network_interface" "nw-interface" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.az_subnet.id
     private_ip_address_allocation = "Dynamic"
+     public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-publicip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  tags                = {
+    environment = "dev"
   }
 }
