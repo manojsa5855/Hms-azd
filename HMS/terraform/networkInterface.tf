4f3bb8d49038aa1702b52027ef6654371d7354a8
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-04-20 09:50:20 +0530

    Update networkInterface.tf

--- HMS/terraform/networkInterface.tf
+++ HMS/terraform/networkInterface.tf
@@ -2,15 +2,23 @@ resource "azurerm_network_interface" "nw-interface" {
   name                = "example-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
+  
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.az_subnet.id
     private_ip_address_allocation = "Dynamic"
      public_ip_address_id          = azurerm_public_ip.example.id
+
+ 
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id = azurerm_network_interface.nw-interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  
+}
 resource "azurerm_public_ip" "example" {
   name                = "example-publicip"
   location            = var.location
@@ -19,4 +27,23 @@ resource "azurerm_public_ip" "example" {
   tags                = {
     environment = "dev"
   }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
