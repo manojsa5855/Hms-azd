5baf2b69f37d30bddd0752d28ca6ee8a199a0422
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-04-20 10:27:34 +0530

    Update networkInterface.tf

--- HMS/terraform/networkInterface.tf
+++ HMS/terraform/networkInterface.tf
@@ -45,16 +45,5 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-    security_rule {
-    name                       = "alltraffic"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 
 }
